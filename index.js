0a3aee818c8e5818665d2a3a7e7f7fa4cec32d47
Hamza Saif <hamzasaiff@gmail.com>
2021-09-29 15:38:41 +0500

    Push Notifications with web-push

--- index.js
+++ index.js
@@ -11,9 +11,8 @@ app.use(express.static(path.join(__dirname, 'client')));
 
 app.use(express.json());
 
-const publicVapidKey =
-  'BEB7MJzspwAGhey--xJct2jf-75p-T_mq5PXQ7-qoac4L_3gPOIujEzLIHd5shFlf_euoO4W1HwXeme4p4ON02A';
-const privateVapidKey = '0VJcnlGkRgbTts7iZFKfiFSJ8m2Xc5EMchtvXznd8Lg';
+const publicVapidKey = process.env.VAPID_PUBLIC_KEY;
+const privateVapidKey = process.env.VAPID_PRIVATE_KEY;
 
 webPush.setVapidDetails('mailto:test@test.com', publicVapidKey, privateVapidKey);
 
